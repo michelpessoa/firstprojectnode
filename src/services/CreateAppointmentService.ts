4ea8d1cc3c748f190db4ee5995d88741436999a6
Michel Pessoa Keursten <michelpessoa@gmail.com>
2020-08-31 10:52:27 -0300

    Services, Repositories Initialized

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import { startOfHour } from "date-fns";
+import { getCustomRepository } from "typeorm";
+
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import Appointment from "../models/Appointment";
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentRepository.create({
+      provider,
+      date: appointmentDate
+    });
+
+    await appointmentRepository.save(appointment);
+
+    return appointment;
+  }
+
+}
+
+export default CreateAppointmentService;
